28e878368400841c46d4043cec3ad61af9510956
GGLozanov <glozanov15@gmail.com>
2021-03-12 10:04:59 +0200

    [Update] Added basic main screens

---
+++ hacktues_gg_app/lib/screens/main/ComparisonScreen.dart
@@ -0,0 +1,10 @@
+// part of StatsScreen 4-panel pageview
+
+import 'package:flutter/material.dart';
+
+class ComparisonScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
